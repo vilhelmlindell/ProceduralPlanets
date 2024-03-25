1c0ea082d2614ef715f056aff7c7eb7a0e67cb2b
Vilhelm Lindell <95926200+vilhelmlindell@users.noreply.github.com>
2024-03-25 11:59:30 +0100

    stuff

---
+++ OrbitDisplay.cs
@@ -0,0 +1,27 @@
+//namespace ProceduralPlanets;
+//
+//using Godot;
+//using System;
+//using System.Collections.Generic;
+//
+//[Tool]
+//public partial class OrbitDisplay
+//{
+//	[Export]
+//	public CelestialBody RelativeBody { get; set; }
+//	public List<Vector3> Points { get; set; }
+//	
+//	public override void _Ready()
+//	{
+//	}
+//
+//	// Called every frame. 'delta' is the elapsed time since the previous frame.
+//	public override void _Process(double delta)
+//	{
+//	}
+//
+//	public void UpdateOrbit(IEnumerable<Vector3> points)
+//	{
+//	}
+//}
+//
